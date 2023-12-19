9d9c3277178cab5a5f92226cd4a433a323e342b8
Arman <armangulistani@gmail.com>
2023-12-19 17:29:41 -0500

    Implemented Celsius/Fahrenheit Toggle
    In this commit, I introduced functionality to dynamically switch between Celsius and Fahrenheit temperature units on the UI. By clicking the Celsius button, the displayed temperature now updates to Celsius degrees, providing users with a more versatile experience. Similarly, clicking the Fahrenheit button toggles the temperature display to Fahrenheit units.

--- src/index.js
+++ src/index.js
@@ -19,11 +19,10 @@ const visibilityInfo = document.getElementById("visibility");
 const pressureInfo = document.getElementById("pressure");
 const precipitationInfo = document.getElementById("precipitation");
 
-// AddEventListener
-submitSearch.addEventListener("click", (event) => {
-  // Prevent the default form submission behavior
-  event.preventDefault();
+const celsiusButton = document.querySelector(".details__degree-celsius");
+const fahrenheitButton = document.querySelector(".details__degree-fahrenheit");
 
+function fetchAndShowData() {
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=2539b1e0094247a1aa3160336231212&q=${searchInput.value}`
   )
@@ -31,85 +30,128 @@ submitSearch.addEventListener("click", (event) => {
       return response.json();
     })
     .then((response) => {
-      // The city name
-      cityName.textContent = response.location.name;
-      // The time
-      date.textContent = response.location.localtime;
-      // The weather condition name
-      weatherCondition.textContent = `Weather condition: ${response.current.condition.text}`;
-      // The current weather temperature
-      temperature.textContent = `${response.current.temp_c}°`;
-      // What the temperature feels like
-      temperatureFeeling.textContent = `Feels like: ${response.current.feelslike_c}°`;
-
-      // The weather icon
-      if (response.current.condition.text === "Sunny") {
-        weatherIcon.textContent = "☀️";
-      } else if (
-        response.current.condition.text === "Cloudy" ||
-        response.current.condition.text === "Overcast" ||
-        response.current.condition.text === "Partly cloudy"
-      ) {
-        weatherIcon.textContent = "☁️";
-      } else if (
-        response.current.condition.text === "Light rain" ||
-        response.current.condition.text === "Patchy rain possible" ||
-        response.current.condition.text === "Mist"
-      ) {
-        weatherIcon.textContent = "🌧";
-      } else if (
-        response.current.condition.text === "Patchy light rain with thunder" ||
-        "Moderate or heavy rain with thunder"
-      ) {
-        weatherIcon.textContent = "⛈";
-      } else if (response.current.condition.text === "Clear") {
-        weatherIcon.textContent = "🌌";
-      } else if (
-        response.current.condition.text === "Snow" ||
-        response.current.condition.text === "Patchy snow possible"
-      ) {
-        weatherIcon.textContent = "❄️";
-      } else if (response.current.condition.text === "Fog") {
-        weatherIcon.textContent = "🌫";
-      } else if (response.current.condition.text === "Freezing fog") {
-        weatherIcon.textContent = "🥶";
-      } else if (
-        response.current.condition.text === "Heavy rain" ||
-        response.current.condition.text === "Moderate or heavy rain shower"
-      ) {
-        weatherIcon.textContent = "☔";
-      } else if (response.current.condition.text === "Thunderstorm") {
-        weatherIcon.textContent = "🌩";
-      } else if (
-        response.current.condition.text === "Hail" ||
-        response.current.condition.text === "Moderate or heavy sleet"
-      ) {
-        weatherIcon.textContent = "🌨";
-      } else if (
-        response.current.condition.text === "Dust" ||
-        response.current.condition.text === "Patchy light drizzle"
-      ) {
-        weatherIcon.textContent = "🌫";
-      } else if (
-        response.current.condition.text === "Haze" ||
-        response.current.condition.text === "Smoke"
-      ) {
-        weatherIcon.textContent = "🌫";
-      } else {
-        weatherIcon.textContent = "❓"; // Unknown or other conditions
-      }
-
-      // The UV Index
-      UVIndexInfo.textContent = `${response.current.uv}`;
-      // The wind status (speed)
-      windStatusInfo.textContent = `${response.current.wind_kph} km/h`;
-      // Humidity
-      humidityInfo.textContent = `${response.current.humidity} %`;
-      // Visibility
-      visibilityInfo.textContent = `${response.current.vis_km} km`;
-      // Pressure
-      pressureInfo.textContent = `${response.current.pressure_in}%`;
-      // Precipitation
-      precipitationInfo.textContent = `${response.current.precip_mm} mm`;
+      left(response);
+      verifyWeatherCondition(response);
+      right(response);
     });
+}
+
+// Search for place event listener
+submitSearch.addEventListener("click", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  fetchAndShowData();
 });
+
+let celsius = true;
+let fahreinheit = false;
+
+function left(APIObject) {
+  // The city name
+  cityName.textContent = APIObject.location.name;
+  // The time
+  date.textContent = APIObject.location.localtime;
+  // The weather condition name
+  weatherCondition.textContent = `Weather condition: ${APIObject.current.condition.text}`;
+
+  if (celsius === true) {
+    // The current weather temperature
+    temperature.textContent = `${APIObject.current.temp_c}°`;
+    // What the temperature feels like
+    temperatureFeeling.textContent = `Feels like: ${APIObject.current.feelslike_c}°`;
+  } else if (fahreinheit === true) {
+    // The current weather temperature
+    temperature.textContent = `${APIObject.current.temp_f}°`;
+    // What the temperature feels like
+    temperatureFeeling.textContent = `Feels like: ${APIObject.current.feelslike_f}°`;
+  }
+}
+
+celsiusButton.addEventListener("click", () => {
+  celsiusButton.classList.add("button-active");
+  fahrenheitButton.classList.remove("button-active");
+  celsius = true;
+  fahreinheit = false;
+  fetchAndShowData();
+});
+
+fahrenheitButton.addEventListener("click", () => {
+  fahrenheitButton.classList.add("button-active");
+  celsiusButton.classList.remove("button-active");
+  fahreinheit = true;
+  celsius = false;
+  fetchAndShowData();
+});
+
+function right(APIObject) {
+  // The UV Index
+  UVIndexInfo.textContent = `${APIObject.current.uv}`;
+  // The wind status (speed)
+  windStatusInfo.textContent = `${APIObject.current.wind_kph} km/h`;
+  // Humidity
+  humidityInfo.textContent = `${APIObject.current.humidity} %`;
+  // Visibility
+  visibilityInfo.textContent = `${APIObject.current.vis_km} km`;
+  // Pressure
+  pressureInfo.textContent = `${APIObject.current.pressure_in}%`;
+  // Precipitation
+  precipitationInfo.textContent = `${APIObject.current.precip_mm} mm`;
+}
+
+function verifyWeatherCondition(APIObject) {
+  // The weather icon
+  if (APIObject.current.condition.text === "Sunny") {
+    weatherIcon.textContent = "☀️";
+  } else if (
+    APIObject.current.condition.text === "Cloudy" ||
+    APIObject.current.condition.text === "Overcast" ||
+    APIObject.current.condition.text === "Partly cloudy"
+  ) {
+    weatherIcon.textContent = "☁️";
+  } else if (
+    APIObject.current.condition.text === "Light rain" ||
+    APIObject.current.condition.text === "Patchy rain possible" ||
+    APIObject.current.condition.text === "Mist"
+  ) {
+    weatherIcon.textContent = "🌧";
+  } else if (
+    APIObject.current.condition.text === "Patchy light rain with thunder" ||
+    "Moderate or heavy rain with thunder"
+  ) {
+    weatherIcon.textContent = "⛈";
+  } else if (
+    APIObject.current.condition.text === "Snow" ||
+    APIObject.current.condition.text === "Patchy snow possible"
+  ) {
+    weatherIcon.textContent = "❄️";
+  } else if (APIObject.current.condition.text === "Fog") {
+    weatherIcon.textContent = "🌫";
+  } else if (APIObject.current.condition.text === "Freezing fog") {
+    weatherIcon.textContent = "🥶";
+  } else if (
+    APIObject.current.condition.text === "Heavy rain" ||
+    APIObject.current.condition.text === "Moderate or heavy rain shower"
+  ) {
+    weatherIcon.textContent = "☔";
+  } else if (APIObject.current.condition.text === "Thunderstorm") {
+    weatherIcon.textContent = "🌩";
+  } else if (
+    APIObject.current.condition.text === "Hail" ||
+    APIObject.current.condition.text === "Moderate or heavy sleet"
+  ) {
+    weatherIcon.textContent = "🌨";
+  } else if (
+    APIObject.current.condition.text === "Dust" ||
+    APIObject.current.condition.text === "Patchy light drizzle"
+  ) {
+    weatherIcon.textContent = "🌫";
+  } else if (
+    APIObject.current.condition.text === "Haze" ||
+    APIObject.current.condition.text === "Smoke"
+  ) {
+    weatherIcon.textContent = "🌫";
+  } else {
+    weatherIcon.textContent = "❓"; // Unknown or other conditions
+  }
+}
