cce04271e68864c29c9db85b069f4af735716cc4
Arman <armangulistani@gmail.com>
2023-12-19 14:05:49 -0500

    Implemented dynamic weather icon based on conditions

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const searchInput = document.getElementById("searchInput");
 const submitSearch = document.querySelector(".main__submit-search");
 
 // AddEventListener
-submitSearch.addEventListener("click", () => {
+submitSearch.addEventListener("click", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=2539b1e0094247a1aa3160336231212&q=${searchInput.value}`
   )
@@ -28,8 +31,65 @@ submitSearch.addEventListener("click", () => {
       // The weather condition name
       weatherCondition.textContent = `Weather condition: ${response.current.condition.text}`;
       // The current weather temperature
-      temperature.textContent = response.current.temp_c + "°";
+      temperature.textContent = `${response.current.temp_c}°`;
       // What the temperature feels like
       temperatureFeeling.textContent = `Feels like: ${response.current.feelslike_c}°`;
+
+      // The weather icon
+      if (response.current.condition.text === "Sunny") {
+        weatherIcon.textContent = "☀️";
+      } else if (
+        response.current.condition.text === "Cloudy" ||
+        response.current.condition.text === "Overcast" ||
+        response.current.condition.text === "Partly cloudy"
+      ) {
+        weatherIcon.textContent = "☁️";
+      } else if (
+        response.current.condition.text === "Light rain" ||
+        response.current.condition.text === "Patchy rain possible" ||
+        response.current.condition.text === "Mist"
+      ) {
+        weatherIcon.textContent = "🌧";
+      } else if (
+        response.current.condition.text === "Patchy light rain with thunder" ||
+        "Moderate or heavy rain with thunder"
+      ) {
+        weatherIcon.textContent = "⛈";
+      } else if (response.current.condition.text === "Clear") {
+        weatherIcon.textContent = "🌌";
+      } else if (
+        response.current.condition.text === "Snow" ||
+        response.current.condition.text === "Patchy snow possible"
+      ) {
+        weatherIcon.textContent = "❄️";
+      } else if (response.current.condition.text === "Fog") {
+        weatherIcon.textContent = "🌫";
+      } else if (response.current.condition.text === "Freezing fog") {
+        weatherIcon.textContent = "🥶";
+      } else if (
+        response.current.condition.text === "Heavy rain" ||
+        response.current.condition.text === "Moderate or heavy rain shower"
+      ) {
+        weatherIcon.textContent = "☔";
+      } else if (response.current.condition.text === "Thunderstorm") {
+        weatherIcon.textContent = "🌩";
+      } else if (
+        response.current.condition.text === "Hail" ||
+        response.current.condition.text === "Moderate or heavy sleet"
+      ) {
+        weatherIcon.textContent = "🌨";
+      } else if (
+        response.current.condition.text === "Dust" ||
+        response.current.condition.text === "Patchy light drizzle"
+      ) {
+        weatherIcon.textContent = "🌫";
+      } else if (
+        response.current.condition.text === "Haze" ||
+        response.current.condition.text === "Smoke"
+      ) {
+        weatherIcon.textContent = "🌫";
+      } else {
+        weatherIcon.textContent = "❓"; // Unknown or other conditions
+      }
     });
 });
